779a0ce7cf20a33f251cbf5a62386017b862f516
bryanares <bryanares12@gmail.com>
2023-02-06 09:07:16 +0300

    create Database

--- app/src/main/java/com/brian/pixit/data/PhotoDatabase.kt
+++ app/src/main/java/com/brian/pixit/data/PhotoDatabase.kt
@@ -1,4 +1,29 @@
 package com.brian.pixit.data
 
-class PhotoDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Photo::class], version = 1, exportSchema = false)
+abstract class PhotoDatabase : RoomDatabase() {
+    abstract fun photoDao() : PhotoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: PhotoDatabase? = null
+        fun getDatabase(context: Context): PhotoDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PhotoDatabase::class.java,
+                    "pixit_photo_db"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
