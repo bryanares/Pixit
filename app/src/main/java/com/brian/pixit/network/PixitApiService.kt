e8a4cbe902d5b4792cb2894602f65070a942c9a4
bryanares <bryanares12@gmail.com>
2023-02-06 09:13:38 +0300

    create API service to fetch images

--- app/src/main/java/com/brian/pixit/network/PixitApiService.kt
+++ app/src/main/java/com/brian/pixit/network/PixitApiService.kt
@@ -1,4 +1,27 @@
 package com.brian.pixit.network
 
-class PixitApiService {
+import com.brian.pixit.models.Photo
+import com.brian.pixit.models.PhotoItem
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://picsum.photos"
+
+interface PixitApiService {
+    @GET("/v2/list")
+    fun getPhotos(): Call<Photo>
+}
+
+object PixitApi{
+    private val retrofit : Retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .baseUrl(BASE_URL)
+        .build()
+
+    val apiService : PixitApiService by lazy {
+        retrofit.create(PixitApiService::class.java)
+    }
 }
