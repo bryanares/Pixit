60aedd9acc0a4a7637db1827fce69b8191beecac
bryanares <bryanares12@gmail.com>
2023-02-06 09:17:36 +0300

    create custom Application class to use created Room Database

--- app/src/main/java/com/brian/pixit/PixitPhotoApplication.kt
+++ app/src/main/java/com/brian/pixit/PixitPhotoApplication.kt
@@ -1,4 +1,8 @@
 package com.brian.pixit
 
-class PixitPhotoApplication {
+import android.app.Application
+import com.brian.pixit.data.PhotoDatabase
+
+class PixitPhotoApplication : Application() {
+    val database : PhotoDatabase by lazy { PhotoDatabase.getDatabase(this) }
 }
